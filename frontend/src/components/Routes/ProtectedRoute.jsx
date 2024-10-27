af583703f1cb4ccb52ead7766d2f239665617298
anmol <anmolof10gms@gmail.com>
2024-10-27 17:48:09 +0530

    fixed bug of protected route

--- frontend/src/components/Routes/ProtectedRoute.jsx
+++ frontend/src/components/Routes/ProtectedRoute.jsx
@@ -3,26 +3,33 @@ import { useSelector } from "react-redux";
 import { Route, useLocation, useNavigate } from "react-router-dom";
 import Loader from "../ui/Loader";
 
-const ProtectedRoute = ({ children, isAdmin  }) => {
+const ProtectedRoute = ({ children, isAdmin }) => {
   const { loading, isAuthenticated, user } = useSelector((state) => state.user);
   const location = useLocation();
   const navigate = useNavigate();
 
-
- 
   useEffect(() => {
     if (!loading) {
       if (isAuthenticated === false) {
-        return navigate("/login", {state: {from: location.pathname}});
+        return navigate("/login", { state: { from: location.pathname } });
       }
-      if(isAdmin === true && user.role !== "admin") {
-        return navigate("/login", {state: {from: location.pathname}});
+      if (isAdmin === true && user.role !== "admin") {
+        return navigate("/login", { state: { from: location.pathname } });
       }
-      
     }
-  }, [])
+  }, [loading, isAuthenticated, navigate, location]);
 
-  return children;
+  return (
+    <>
+      {isAuthenticated === false ? (
+        <div className="flex items-center justify-center w-full h-screen">
+          <Loader />
+        </div>
+      ) : (
+        children
+      )}
+    </>
+  );
 };
 
 export default ProtectedRoute;
